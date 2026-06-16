feba860390040c4c0ae82198e3ea0199a021f9f2
LugoBlogger <lugoblogger@gmail.com>
2026-06-16 11:11:08 +0700

    finish the program in small-world-phenomenon

--- case-studies/4-small-world-phenomenon/python/performer.py
+++ case-studies/4-small-world-phenomenon/python/performer.py
@@ -17,17 +17,17 @@ def _main():
     names = line.split(delimiter)
     for i in range(1, len(names)):
       for j in range(i+1, len(names)):
-        g.add_edge(names[i], names[j])
+        g.add_edge(names[i].strip(), names[j].strip())
 
   # print(g)
   degree = smallworld.average_degree(g)
-  length = smallworld.average_path_length(g)
-  cluster = smallworld.clustering_coefficient(g)
+  # length = smallworld.average_path_length(g)
+  # cluster = smallworld.clustering_coefficient(g)
 
   print(f"number of vertices     = {g.count_v():3d}")
   print(f"average degree         = {degree:7.3f}")
-  print(f"average path length    = {length:7.3f}")
-  print(f"clustering coefficient = {cluster:7.3f}")
+  # print(f"average path length    = {length:7.3f}")
+  # print(f"clustering coefficient = {cluster:7.3f}")
 
 
 
